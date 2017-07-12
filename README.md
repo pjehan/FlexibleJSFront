585144ba6d86abe5f8598ded653494d28a0b5aba
pjehan <pierre.jehan@gmail.com>
2017-07-12 13:20:52 +0200

    ReCaptcha

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
 # FlexibleJSFront
+
+## Table of contents
+- [ReCaptcha](#recaptcha)
+
+## ReCaptcha
+To use Google ReCaptcha on your website, generate a new private and public key on the [Google ReCaptcha](https://www.google.com/recaptcha/admin) admin website.  
+Then, copy and paste the public and private keys in the config.js file:
+```
+recaptcha: {
+  sitekey: 'RECAPTCHA-SITEKEY',
+  secretkey: 'RECAPTCHA-SECRETKEY'
+}
+```
+Now you can use the public key in your HTML forms:
+```jade
+.g-recaptcha(data-sitekey='#{config.recaptcha.sitekey}')
+```
+And check the captcha validity in the routes/emails.js file:
+```javascript
+checkReCaptcha(req).then((response) => {
+  res.mailer.send('emails/contact', {
+    replyTo: req.body.email,
+    to: req.app.locals.config.mailer.to,
+    subject: 'Contact Form',
+    form: req.body
+  }, function (err) {
+    if (err) {
+      return next(err);
+    }
+    res.send('Email Sent');
+  });
+}, (error) => {
+  return next(error);
+});
+```
--- routes/emails.js
+++ routes/emails.js
@@ -1,8 +1,70 @@
 'use strict';
 
 var express = require('express');
+var http = require("http");
+var https = require("https");
+var querystring = require('querystring');
 var router = express.Router();
 
+function checkReCaptcha(req) {
 
+  return new Promise((resolve, reject) => {
+    var postData = querystring.stringify({
+      'secret': req.app.locals.config.recaptcha.secretkey,
+      'response': req.body['g-recaptcha-response']
+    });
+
+    var options = {
+      host: 'www.google.com',
+      port: 443,
+      path: '/recaptcha/api/siteverify',
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Content-Length': Buffer.byteLength(postData)
+      }
+    };
+
+    var recaptchaReq = https.request(options, (res) => {
+      res.setEncoding('utf8');
+      res.on('data', (chunk) => {
+        var data = JSON.parse(chunk);
+        if (data.success) {
+          resolve();
+        } else {
+          reject();
+        }
+      });
+    });
+
+    recaptchaReq.on('error', (e) => {
+      reject(e);
+    });
+
+    recaptchaReq.write(postData);
+    recaptchaReq.end();
+  });
+
+}
+
+router.post('/contact', function (req, res, next) {
+
+  checkReCaptcha(req).then((response) => {
+    res.mailer.send('emails/contact', {
+      replyTo: req.body.email,
+      to: req.app.locals.config.mailer.to, // TODO: Query DB
+      subject: 'Contact form',
+      form: req.body
+    }, function (err) {
+      if (err) {
+        return next(err);
+      }
+      res.send('Email Sent');
+    });
+  }, (error) => {
+    return next(error);
+  });
+
+});
 
 module.exports = router;
